e3fb9ffc48274e3395d94118a20cdd11dc268557
Eugene Paitoo <eugenepaitoo@gmail.com>
2023-09-06 21:43:35 +0000

    add middleware to routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func (app *application) routes() *http.ServeMux {
+	"github.com/justinas/alice"
+)
+
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -12,6 +16,9 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/reminder/view", app.reminderView)
 	mux.HandleFunc("/reminder/create", app.reminderCreate)
 
-	return mux
+	// middleware chain for every request in the app
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	return standard.Then(mux)
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/epaitoo/remindme
 
 go 1.21.0
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require github.com/go-sql-driver/mysql v1.7.1
+
+require github.com/justinas/alice v1.2.0
